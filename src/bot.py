48cc519e325ea0ed4594260c836605b70fe79941
Misha Ivkov <applepi216@gmail.com>
2018-11-10 13:39:12 -0500

    initial bot

--- src/bot.py
+++ src/bot.py
@@ -1,23 +1,49 @@
-class Bot(object):
+import abc
 
-    def __init__(self,x,y,step_gain):
-        self.x = x
-        self.y = y
+class Bot(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, position, step_gain, speed):
+        self.tile = position
         self.step_gain = step_gain
         self.progress = 0
-        self.should_advance = false
+        self.dest_tile = None
+        self.speed = speed
 
-    def set_destinations(self, dest_x, dest_y):
-        self.dest_x = dest_x
-        self.dest_y = dest_y
+    def update_progress(self):
+        if self.progress >= self.dest_tile.threshold:
+            self.tile = self.dest_tile
+            # TODO(Cuebeom): Update bots in tile class
+            self.progress = 0
+            self.dest_tile = None
+        else:
+            self.progress += self.speed
 
-    def set_progress(self, progress):
-        self.progress = progress
+    @abc.abstractmethod
+    def compute_step(self, board):
+        """
+        Bot should set dest_tile
+        """
+        raise "Superclass must implement compute_step"
 
-    def compute_step(self,threshold):
-        self.progress += self.step_gain
-        if self.progress >= threshold:
-            should_advance = true
+    def compute_progress(self, board):
+        new_dest = self.dest_tile
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+        compute_step(self,board)
+=======
+        compute_step(self)
+>>>>>>> Stashed changes
+=======
+        compute_step(self)
+>>>>>>> Stashed changes
+        if new_dest != self.dest_tile:
+            self.progress = 0
 
-    def execute_step(self):
-        #needs to know next tile, will code later
+    def execute_step(self, board, new_dest=None):
+        if new_dest is not None:
+            self.tile = new_dest
+            self.dest_tile = None
+            self.progress = 0
+        elif dest_tile is not None:
+            update_progress(self)
