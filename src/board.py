73ab46d7e57d28068367c8a5381450b2ea6d46a1
Harry <hjx@andrew.cmu.edu>
2018-11-17 15:18:06 -0500

    Added verification for straight booths only

--- src/board.py
+++ src/board.py
@@ -73,6 +73,9 @@ class Board(object):
         # type(self.num_companies) = dictionary where each entry has an
         #     integer corresponding to the number of booths it has
         # type(self.board) = numpy.ndarray (2D Array)
+        self.parse(company_config_file, board_config_file)
+
+    def parse(self, company_config_file, board_config_file):
         self.parse_companies(company_config_file)
         self.parse_board(board_config_file)
 
@@ -115,6 +118,7 @@ class Board(object):
             # create all tiles
             for i in range(0, num_rows):
                 for j in range(0, num_cols):
+                    # TODO: Change from 0
                     board[i][j] = Tile(i, j, 0)
 
             # copy string for multiple passes
@@ -129,10 +133,12 @@ class Board(object):
             # create all booths
             max_comp = 0
             max_line = 0
+            seen_comp = set()
+            seen_lines = set()
 
-            for i in range(0, rows):
-                for j in range(0, cols):
-                    char = line[i][j]
+            for i in range(0, num_rows):
+                for j in range(0, num_cols):
+                    char = board_copy[i][j]
                     if char == "X":
                         pass
                     elif char == "F":
@@ -141,19 +147,68 @@ class Board(object):
                         raise Exception("Got empty spot at", i, j)
                     elif char[0].isnumeric():
                         # we have a line
-                        max_line = max(int(char[0]), max_line)
+                        index = int(char)
+                        max_line = max(index, max_line)
+                        seen_lines.add(index)
                     elif char[0] == "C":
-                        max_comp = max(int(char[1:]), max_comp)
+                        index = int(char[1:])
+                        max_comp = max(index, max_comp)
+                        seen_comp.add(index)
                     elif char[0] == "E":
-                        max_line = max(int(char[1:]), max_line)
+                        index = int(char[1:])
+                        max_line = max(index, max_line)
+                        seen_lines.add(index)
                     else:
                         raise Exception("Unknown tile char at", i, j)
 
             if max_comp != max_line:
                 raise Exception("Max company and max lines don't match, got",
                   max_comp, max_line)
+            if max_comp > len(self.company_infos):
+                raise Exception("Found more company spots than company informations",
+                "got", max_comp, "expected at most", len(self.company_infos))
+            seen_comp = sorted(list(seen_comp))
+            seen_lines = sorted(list(seen_lines))
+
+            if len(seen_comp) < 2 or (seen_comp[0] == 0 and
+              seen_comp[-1] == max_comp and len(seen_comp) == max_comp):
+                pass
+            else:
+                raise Exception("Missing company", seen_comp, "max", max_comp)
+
+            if len(seen_lines) < 2 or (seen_lines[0] == 0 and
+              seen_lines[-1] == max_lines and len(seen_lines) == max_lines):
+                pass
+            else:
+                raise Exception("Missing lines", seen_lines, "max", max_line)
+
 
             # create all lines
+            # we reserve the first index for the end
+            lines = [[(-1, -1)] for i in range(max_line)]
+            booths = [[] for i in range(max_comp)]
+
+            for i in range(0, num_rows):
+                for j in range(0, num_cols):
+                    char = board_copy[i][j]
+                    # should have correctness checked earlier
+                    if char[0].isnumeric():
+                        index = int(char)
+                        lines[index].append((i, j))
+                    elif char[0] == "C":
+                        index = int(char[1:])
+                        booths[index].append((i, j))
+                    elif char[0] == "E":
+                        index = int(char[1:])
+                        if line[index][0][0] != -1 and line[index][0][1] != -1:
+                            raise Exception("Multiple line endings for company",
+                              index)
+                        lines[index][0] = (i, j)
+
+            # formats and sets lines/booths
+            # our inputted lists are not well organized
+            self.set_booths(booths)
+            self.set_lines(lines)
 
             # set tiles with booths and lines
             for i in range(rows):
@@ -167,8 +222,6 @@ class Board(object):
                         raise Exception("Unmovable spots not implemented")
                     elif char == "F":
                         pass
-                    elif char == "":
-                        raise Exception("Got empty spot")
                     elif char[0].isnumeric():
                         pass
                     elif char[0] == "C":
@@ -177,3 +230,82 @@ class Board(object):
                         pass
 
             self.board = board
+
+    def set_booths(self, booths):
+        # we have a collection of booth coordinates, we need to find start/end
+        def format_booth(booth):
+            if len(booth) < 2:
+                return booth
+
+            booth_set = set(booth)
+            start = (-1, -1)
+
+            i = 0
+            while not found and i < len(booth):
+                cell = booth[i]
+                neighbors = 0
+                cell_ll = (cell[0], cell[1] - 1)
+                cell_rr = (cell[0], cell[1] + 1)
+                cell_dn = (cell[0] + 1, cell[1])
+                cell_up = (cell[0] - 1, cell[1])
+
+                if cell_up in booth_set:
+                    neighbors += 1
+                if cell_dn in booth_set:
+                    neighbors += 1
+                if cell_rr in booth_set:
+                    neighbors += 1
+                if cell_ll in booth_set:
+                    neighbors += 1
+
+                if neighbors == 0:
+                    raise Exception("Got singleton island booth in multiple tiles",
+                      booth)
+                elif neighbors == 1:
+                    start = cell
+                    found = True
+                elif neighbors == 2:
+                    pass
+                elif neighbors > 2:
+                    raise Exception("Tree based booth structure?", booth)
+
+                i += 1
+
+            start_up = (start[0] + 1, start[1])
+            start_dn = (start[0] - 1, start[1])
+            start_rr = (start[0], start[1] + 1)
+            start_ll = (start[0], start[1] - 1)
+
+            dir = None
+            if start_up in booth_set:
+                dir = (1, 0)
+            elif start_dn in booth_set:
+                dir = (-1, 0)
+            elif start_rr in booth_set:
+                dir = (0, 1)
+            elif start_ll in booth_set:
+                dir = (0, -1)
+
+            if dir == None:
+                raise Exception("Can't find a direction for next block")
+
+            curr = start
+            booth_tiles_seen = set([curr])
+            res = [curr]
+            while curr in booth_set:
+                curr = (curr[0] + dir[0], curr[1] + dir[1])
+                booth_tiles_seen.add(curr)
+                res.append(curr)
+
+            if len(booth_tiles_seen) != len(booth):
+                raise Exception("Haven't parsed every line tile",
+                  "got", booth_tiles_seen, "expected", booth)
+
+            return res
+
+        self.booths = []
+        for indv_booth in booths:
+            self.booths.append(format_booth(indv_booth))
+
+    def set_lines(self, lines):
+        pass
