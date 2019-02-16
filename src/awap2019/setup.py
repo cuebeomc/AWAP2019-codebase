8cf9a061b2abaf5e981414eec5ecb79ed38c8c7e
Cuebeom Choi <cuebeom.choi@gmail.com>
2019-02-16 12:13:35 -0500

    finalized code.

--- src/awap2019/setup.py
+++ src/awap2019/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='awap2019',
-      version='1.2.9',
+      version='2.0',
       description='Module for AWAP 2019.',
       long_description='Contains essential classes for competitors.',
       url='http://github.com/cuebeomc/awap2019',
