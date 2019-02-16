60805babac37910737a7265706121888e559a2f8
Cuebeom Choi <cuebeom.choi@gmail.com>
2019-02-16 10:18:46 -0500

    Cleaning up scripts

--- distribution/player/team.py
+++ distribution/player/team.py
@@ -1,46 +1,40 @@
-"""This is a general template as to how the competitors will code.
+"""
+The team.py file is where you should write all your code!
 
-They will be given the initial board (the 2D grid of tiles that contain
-rudimentary information, allowing them to map out a path that they would like
-to take), and at every step, they will be passed in a 2D grid with a board
-with more information pertaining to their locations and a list of positions
-that is ordered (index 0 contains the main bot's loc, index 1 contains
-1st helper's loc, etc)."""
+Write the __init__ and the step functions. Further explanations
+about these functions are detailed in the wiki.
 
-from awap2019 import Tile, Direction
-from awap2019.bots import Bot
+List your Andrew ID's up here!
+andrewid1
+andrewid2
+andrewid3
+andrewid4
+"""
+from awap2019 import Tile, Direction, State
 
 class Team(object):
-    # initial_board is a 2d grid full of tiles of booth information, but no bots or lines!
-    # team_size is how many bots you should expect (for competition purposes we'll have 4)
     def __init__(self, initial_board, team_size, company_info):
-        # Feel free to do some work with the initial board!
+        """
+        The initializer is for you to precompute anything from the
+        initial board and the company information! Feel free to create any
+        new instance variables to help you out.
+
+        Specific information about initial_board and company_info are
+        on the wiki. team_size, although passed to you as a parameter, will
+        always be 4.
+        """
         self.board = initial_board
         self.team_size = team_size
         self.company_info = company_info
 
-    def step(self, visible_board, states, score):
-        """This function should return a list of directions. For example,
-        [Direction.RIGHT, Direction.LEFT, Direction.NONE]
-        indicates that the main bot is told to go right, the first helper
-        bot goes left, and the second helper bot does nothing.
-        """
-        moves = []
-        for i in range(self.team_size):
+        self.team_name = # Add your team name here!
 
-            user_input = input("\nMovement {}: ".format(i))
-            if user_input == 'left':
-                moves.append(Direction.LEFT)
-            elif user_input == 'right':
-                moves.append(Direction.RIGHT)
-            elif user_input == 'up':
-                moves.append(Direction.UP)
-            elif user_input == 'down':
-                moves.append(Direction.DOWN)
-            elif user_input == 'enter':
-                moves.append(Direction.ENTER)
-            elif user_input == 'replace':
-                moves.append(Direction.REPLACE)
-            else:
-                moves.append(Direction.NONE)
-        return moves
+    def step(self, visible_board, states, score):
+        """
+        The step function should return a list of four Directions.
+
+        For more information on what visible_board, states, and score
+        are, please look on the wiki.
+        """
+
+        pass
--- test/main.py
+++ test/main.py
@@ -12,28 +12,34 @@ Example:
 from awap2019 import Direction, Game
 from absl import app, flags
 
-from player1.team import Team as P1
+from player.team import Team as P
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('num_moves', 300, 'The number of moves in a game.')
 flags.DEFINE_integer('score_threshold', 200, 'The score needed to win the game')
 
-flags.DEFINE_string('config', 'boards/sample.txt', 'The path to the config file.')
+flags.DEFINE_string('board_directory', 'boards/', 'The directory where the'
+                                                  'the boards are stored.')
+flags.DEFINE_string('log_directory', 'logs/', 'The path to the log file.')
 flags.DEFINE_string('companies', 'companies.txt', 'The path to the list of '
                                                   'companies.')
-flags.DEFINE_string('log_file', 'logs/test.txt', 'The path to the log file.')
+flags.DEFINE_string('board_file', 'tiny', 'The name of the board file.')
+flags.DEFINE_string('output_file', 'out', 'The output file.')
 flags.DEFINE_boolean('debug', False, 'Debug mode')
 
 IDLE_TIME = 10
 TEAM_SIZE = 4
 
 def main(_):
-    g = Game(FLAGS.config, FLAGS.companies, FLAGS.log_file, False,
+    board_file = "{}{}.txt".format(FLAGS.board_directory, FLAGS.board)
+    log_file = "{}{}-{}.txt".format(FLAGS.log_directory, FLAGS.board, FLAGS.output_file)
+
+    g = Game(board_file, FLAGS.companies, log_file, False,
              FLAGS.debug, TEAM_SIZE)
 
     grid = g.generate_player_copy(init=True)
 
-    player1 = P1(grid, FLAGS.team_size, g.get_companyinfo())
+    player1 = P(grid, FLAGS.team_size, g.get_companyinfo())
 
     grid = g.generate_player_copy(team=0)
     state = g.board.get_states(0)
